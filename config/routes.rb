54205bba2ced2891099dc3016c182f88231abb47
Gatis Tomsons <gatis@ithouse.lv>
2009-08-04 16:59:33 +0300

    Routēm updeits, bus bez locale ari

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect  'admin',  :controller=>'admin/configuration', :action=>'list', :path_prefix => '/:locale'
+  map.connect  'system/login',   :controller=>'admin/user',  :action=>'login'
+  map.connect  'system/logout',  :controller=>'admin/user ', :action=>'logout'
   map.connect  'system/login',   :controller=>'admin/user',  :action=>'login', :path_prefix => '/:locale'
   map.connect  'system/logout',  :controller=>'admin/user ', :action=>'logout', :path_prefix => '/:locale'
   map.connect ':controller/:action', :path_prefix => '/:locale'
