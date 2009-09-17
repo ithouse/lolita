6e0fea864ce858ce16a1e12504d5f34815403fda
Gatis Tomsons <gatis@ithouse.lv>
2009-09-17 14:28:39 +0300

    Renamed migrations

--- db/migrate/20090616133307_update_menu_item_with_url.rb
+++
@@ -1,9 +0,0 @@
-class UpdateMenuItemWithUrl < ActiveRecord::Migration
-  def self.up
-    add_column :menu_items, :url, :text
-  end
-
-  def self.down
-    remove_column :menu_items, :url
-  end
-end
