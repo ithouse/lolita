a8f22ec8e1b1ab19cc3c96740b538de11e1205a8
Arturs Meisters <arturs@ithouse.lv>
2009-09-22 09:12:17 +0300

    Flash actions working now

--- lib/flash_session_cookie_middleware.rb
+++ lib/flash_session_cookie_middleware.rb
@@ -10,7 +10,7 @@ class FlashSessionCookieMiddleware
     if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
       req = Rack::Request.new(env)
       puts req.params.to_s
-      if Lolita.config.system(:flash_actions).include?(req.params['action']) && !req.params[@session_key].nil?
+      if Lolita.config.system(:flash_actions).include?(req.path) && !req.params[@session_key].nil?
         env['HTTP_COOKIE'] = "#{@session_key}=#{req.params[@session_key]}".freeze
       end
     end
