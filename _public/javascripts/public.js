7413d339b813343dfc1c86fcd4058203b7097bf8
Gatis Tomsons <gatis@ithouse.lv>
2009-09-22 10:00:30 +0300

    removed gsdata, aded some functions to public.js

---
+++ _public/javascripts/public.js
@@ -0,0 +1,24 @@
+function loadjscssfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+function elementById(id){
+    if (document.getElementById)
+        return (document.getElementById(id));
+    if (document.all)
+        return document.all[id];
+    if (document.layers)
+        return document.layers[id];
+    return false;
+}
