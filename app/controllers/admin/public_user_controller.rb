764e121f6752fda6d2c01dfb4d9f0ec863c4197f
Gatis Tomsons <gatis@ithouse.lv>
2009-10-03 14:34:42 +0300

    Added status code 400 to login_public_user when failing

--- app/controllers/admin/public_user_controller.rb
+++ app/controllers/admin/public_user_controller.rb
@@ -9,12 +9,13 @@ class Admin::PublicUserController < ApplicationController
       loged_in=yield user
       if user && loged_in
         register_user_in_session user
-        return redirect_to options[:url] || home_url
+        redirect_to options[:url] || home_url
       else
         flash[:error]||=I18n.t(:"flash.error.auth failed")
+        render :status => 400
       end
     else
-      return redirect_to options[:url] || home_url if logged_in?
+      redirect_to options[:url] || home_url if logged_in?
     end
   end
 
