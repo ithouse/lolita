0786ef112da4df14670c6b77488e209f712b0760
Gatis Tomsons <gatis@ithouse.lv>
2009-12-09 10:44:49 +0200

    [hotfix] user controller

--- app/controllers/extensions/cms/handle_metadata.rb
+++ app/controllers/extensions/cms/handle_metadata.rb
@@ -15,21 +15,12 @@ module Extensions
       private
 
       def save_metadata_with_translation
-        unless @metadata || @metadata=MetaData.by_metaable(@object.id,@config[:object_name])
-          @metadata=MetaData.new(:metaable_type=>@config[:object_name].camelize,:metaable_id=>@object.id)
+        if !@metadata && !@metadata=MetaData.by_metaable(@object.id,@config[:object_name])
+          @metadata=MetaData.new
         end
-
-        if my_params[:metadata] && @object.respond_to?(:allow_metadata_edit)
-          allowed=@object.send(:allow_metadata_edit)
-          return unless allowed
-          if allowed.is_a?(Hash)
-            allowed.each { |key,val|
-              my_params[:metadata].delete(key) unless val
-            }
-          end
-        end
-
-        if Lolita.config.i18n :translation && my_params[:meta_translation_locale]
+        @metadata.metaable_type=@config[:object_name].camelize
+        @metadata.metaable_id=@object.id
+        if Lolita.config.translation && my_params[:meta_translation_locale]
           #raise Globalize::Wrong language error if first language switched and then saved
           # work good if block given
           @metadata.switch_language(my_params[:meta_translation_locale]) do
@@ -37,7 +28,8 @@ module Extensions
           end
           @metadata.switch_language(my_params[:meta_translation_locale]) #this switch object lang
         else
-          @metadata.update_attributes!(my_params[:metadata])
+          my_params[:metadata].each{|k,v| @metadata.send(:"#{k}=",v)}
+          @metadata.save!
         end
       end
 
