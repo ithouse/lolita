a99d05c9824600fd5812cba9beb7423848cabc43
Gatis Tomsons <gatis@ithouse.lv>
2009-12-15 09:49:33 +0200

    Fixed empty config and generators, setup works

--- generators/lolita/lolita_generator.rb
+++ generators/lolita/lolita_generator.rb
@@ -1,8 +1,10 @@
 class LolitaGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      # m.directory "lib"
-      # m.template 'README', "README"
+      m.file "lolita.yml", "config/lolita.yml"
+      m.file "public.js", "public/javascripts/public.js"
+      m.file "start_lolita.rb", "config/initializers/start_lolita.rb"
+      m.file "tinymcestyle.css", "public/stylesheets/tinymcestyle.css"
     end
   end
 end
