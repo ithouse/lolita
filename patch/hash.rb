470380244bac8b12f319e5b2818b00add3f5ba01
Gatis Tomsons <gatis@ithouse.lv>
2009-12-23 15:20:27 +0200

    Fixed bug delivery

--- patch/hash.rb
+++ patch/hash.rb
@@ -1,4 +1,5 @@
 class ::Hash
+  # converts all keys to symbols, but RECURSIVE
   def symbolize_keys!
     each do |k,v|
       sym = k.respond_to?(:to_sym) ? k.to_sym : k
@@ -6,5 +7,11 @@ class ::Hash
       delete(k) unless k == sym
     end
     self
+  end
+  # converts Hash and HashWithIndifferentAccess to readable HTML like
+  # >> {:a => 1, :b => {:c => 55}}.to_html
+  # => "<ul><li>a => 1</li><li>b => <ul><li>c => 55</li></ul></li></ul>"
+  def to_html
+    "<ul>#{self.collect{|k,v| "<li>#{k} => #{([Hash,HashWithIndifferentAccess].include?(v.class))? v.to_html : v}</li>"}}</ul>"
   end
 end
