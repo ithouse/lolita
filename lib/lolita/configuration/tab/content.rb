d0029c43f055d790ad806176a61296874c9755d1
Arturs Meisters <arturs@ithouse.lv>
2011-08-29 14:42:38 +0300

    default column and field generation updated

--- lib/lolita/configuration/tab/content.rb
+++ lib/lolita/configuration/tab/content.rb
@@ -9,23 +9,7 @@ module Lolita
     	  	set_default_fields
     	  end
 
-        def default_fields
-          self.current_dbi.fields.map{|db_field|
-            exclude_field?(db_field) ? nil : self.field(db_field)
-          }.compact
-        end
-
-        def exclude_field?(db_field)
-          db_field[:name].to_s.match(/^(created_at|updated_at|\w+_count)$/) ||
-          (db_field[:name].match(/^\w+_id$/) && !association_column?(db_field[:name]))
-        end
-
-        def association_column?(name)
-          self.respond_to?(name.gsub(/_id$/,"").to_sym)
-        end
-
     	  private 
-
 
         def set_default_fields
           default_fields if @fields.empty? 
