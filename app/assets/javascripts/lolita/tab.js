89aad14bde94adfd42087814f2d97a77e36c9d46
Aivils Štoss <aivils@ithouse.lv>
2011-11-11 16:15:22 +0200

    fix numpad for integer field

--- app/assets/javascripts/lolita/tab.js
+++ app/assets/javascripts/lolita/tab.js
@@ -44,9 +44,9 @@ $(function(){
     }
     else {
       // Ensure that it is a number and stop the keypress
-      if (event.keyCode < 48 || event.keyCode > 57 ) {
+      if (!((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105))) {
         event.preventDefault(); 
-      }   
+      }
     }
   })
 
