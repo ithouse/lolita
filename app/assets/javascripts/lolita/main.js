283c430a38cff3f56da2229d8e39f68a3681968f
Arturs Meisters <arturs@ithouse.lv>
2011-11-22 20:29:36 +0200

    nested forms style and js updated

--- app/assets/javascripts/lolita/main.js
+++ app/assets/javascripts/lolita/main.js
@@ -46,5 +46,6 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(content.replace(regexp, new_id)).insertBefore($(link).parent());
+  var $container = $(link).parent().siblings(".nested-form-fields-container")
+  $container.prepend($(content.replace(regexp, new_id)))
 }
