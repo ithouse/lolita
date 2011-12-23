7e6f2c0846aa1a4e0fe9e7378992a2429c891188
Arturs Meisters <arturs@ithouse.lv>
2011-12-23 10:23:49 +0200

    history,readme changed

--- lib/lolita/support/version.rb
+++ lib/lolita/support/version.rb
@@ -1,11 +1,19 @@
 module Lolita
   module Support
+    # Lolita version in more friendly way.
+    #     Lolita.version #=> 3.2.0 
+    #     Lolita.version.major #=> 3
+    #     Lolita.version.minor #=> 2
     class Version
       def initialize
         @version = LOLITA_VERSION
       end
 
       def inspect
+        @version
+      end
+
+      def to_s
         @version
       end
 
