d395c74a625869f95a4cadfef0b84e3fcb905b4e
Arturs Meisters <arturs@ithouse.lv>
2012-04-17 13:01:29 +0300

    tests fixed

--- spec/controllers/internal_helpers_spec.rb
+++ spec/controllers/internal_helpers_spec.rb
@@ -4,7 +4,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 class MyController < ApplicationController
-  include Lolita::Controllers::InternalHelpers
+  include Lolita::Controllers::RailsHelpers
   include Lolita::Hooks
   add_hook :before_build_resource,:after_build_resource
   add_hook :before_index
