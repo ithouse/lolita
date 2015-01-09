e4bf5908686510a42d3e32bd3195a9b24c3eba52
Gatis Tomsons <gatis@ithouse.cc>
2015-01-09 18:18:23 +0200

    restored back missig custom column sorting

--- app/helpers/components/lolita/configuration_component.rb
+++ app/helpers/components/lolita/configuration_component.rb
@@ -2,20 +2,19 @@ module Components
   module Lolita
     module ConfigurationComponent
       def link_to_remove_fields(name, f)
-        f.hidden_field(:_destroy) + link_to(name, "#", onclick: "remove_fields(this)")
+        f.hidden_field(:_destroy) + link_to(name, '#', onclick: 'remove_fields(this); return false;')
       end
 
       def link_to_add_fields(name, f, nested_form)
         new_object = nested_form.klass.new
-        fields_content = ""
+        fields_content = ''
         f.fields_for(nested_form.name, new_object, child_index: "new_#{nested_form.name}") do |builder|
-          self.current_form(builder) do
+          current_form(builder) do
             fields_content = render_component(nested_form, :"fields")
           end
         end
-        link_to(name, "#", onclick: "add_fields(this, \"#{nested_form.name}\", \"#{escape_javascript(fields_content)}\")")
+        link_to(name, '', class: 'pointer', onclick: "add_fields(this, \"#{nested_form.name}\", \"#{escape_javascript(fields_content)}\"); return false;")
       end
-
     end
   end
 end
--- lib/lolita/version.rb
+++ lib/lolita/version.rb
@@ -2,7 +2,7 @@ module Lolita
   module Version
     MAJOR = 4
     MINOR = 0
-    PATCH = 2
+    PATCH = 3
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
